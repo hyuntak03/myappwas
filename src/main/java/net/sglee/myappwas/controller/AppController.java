76c00f2390ed3cbc4fed13e2a044b2a6ff8f34a6
hyuntak03 <hyuntak03.lee@gmail.com>
2018-08-12 20:32:52 +0900

    complete controller

--- src/main/java/net/sglee/myappwas/controller/AppController.java
+++ src/main/java/net/sglee/myappwas/controller/AppController.java
@@ -4,13 +4,10 @@ package net.sglee.myappwas.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 //import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 public class AppController {
     private static final Logger logger =
             LoggerFactory.getLogger(AppController.class);
@@ -18,6 +15,7 @@ public class AppController {
     @RequestMapping(
             value = "/msg",
             method = RequestMethod.GET)
+    @CrossOrigin
     @ResponseBody
     public String sendMsg(
             @RequestParam("name") String _name) {
@@ -27,6 +25,6 @@ public class AppController {
 //        } else {
 //            return "no records";
 //        }
-        return "Hello" + _name;
+        return "Hello, " + _name;
     }
 }
